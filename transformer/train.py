5028d8596de1c4b81bb4ae7cd902399ff8b508b8
regmi-saugat <3278633@mbmc.edu.np>
2024-01-20 17:26:14 +0545

    chore: WIP for building tokenizer

---
+++ transformer/train.py
@@ -0,0 +1,29 @@
+import torch
+import torch.nn as nn
+from datasets import load_dataset
+from tokenizers import Tokenizer
+from tokenizers.models import WordLevel
+from tokenizers.trainers import WordLevelTrainer
+from tokenizers.pre_tokenizers import Whitespace
+from pathlib import Path
+
+from .utils import Utils
+
+
+def get_or_build_tokenizers(config, dataset, language):
+    # config["tokenizer_file"] = '../tokenizers/tokenizer_{0}.json'
+    tokenizer_path = Path(config["tokenizer_file"].format(language))
+
+    if not Path.exists(tokenizer_path):
+        tokenizer = Tokenizer(WordLevel(unk_token="[UNK]"))
+        tokenizer.pre_tokenizer = Whitespace()
+        trainer = WordLevelTrainer(
+            special_tokens=["[UNK]", "[PAD]", "[SOS]", "[EOS]"], min_frequency=2
+        )
+        tokenizer.train_from_iterator(
+            Utils.get_all_sentences(dataset, language), trainer=trainer
+        )
+        tokenizer.save(str(tokenizer_path))
+    else:
+        tokenizer = Tokenizer.from_file(str(tokenizer_path))
+    return tokenizer
